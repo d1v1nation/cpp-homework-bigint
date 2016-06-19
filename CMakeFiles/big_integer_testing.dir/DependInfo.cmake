b59ef21916f653230a59c2b81613ede2c14dcd8c
d1v1nation <catanaut@gmail.com>
2016-06-19 14:20:14 +0300

    Initial commit

---
+++ CMakeFiles/big_integer_testing.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cat/inst/asm-cpp/cpp-homework-bigint/clion-proj/big_integer.cpp" "/home/cat/inst/asm-cpp/cpp-homework-bigint/clion-proj/CMakeFiles/big_integer_testing.dir/big_integer.cpp.o"
+  "/home/cat/inst/asm-cpp/cpp-homework-bigint/clion-proj/big_integer_testing.cpp" "/home/cat/inst/asm-cpp/cpp-homework-bigint/clion-proj/CMakeFiles/big_integer_testing.dir/big_integer_testing.cpp.o"
+  "/home/cat/inst/asm-cpp/cpp-homework-bigint/clion-proj/gtest/gtest-all.cc" "/home/cat/inst/asm-cpp/cpp-homework-bigint/clion-proj/CMakeFiles/big_integer_testing.dir/gtest/gtest-all.cc.o"
+  "/home/cat/inst/asm-cpp/cpp-homework-bigint/clion-proj/gtest/gtest_main.cc" "/home/cat/inst/asm-cpp/cpp-homework-bigint/clion-proj/CMakeFiles/big_integer_testing.dir/gtest/gtest_main.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
